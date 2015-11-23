56e69079f19f27a06fe42b6d35251dea15643e3d
Dimitri Bernot <dimitri.bernot@yahoo.fr>
2015-11-23 15:02:13 +0100

    Fichiers generation de carte

---
+++ src/carte.c
@@ -0,0 +1,22 @@
+#define TAILLE_SALLE 10
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void carteCharger() {
+	FILE * salle;
+	
+	salle=fopen("./map/test_map.txt", "r");
+	
+	if(salle != NULL) {
+		fclose(salle);
+		printf("Parfait !");
+	} else {
+		printf("Meh");
+	}
+}
+
+int main() {
+	carteCharger();
+	return 0;
+}
