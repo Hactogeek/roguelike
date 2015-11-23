44bc00829f886c12eff882c34b897f90201ed78b
Marvin <callof61@gmail.com>
2015-11-23 15:12:53 +0100

    Ajout du VRAI main

--- src/main.c
+++ src/main.c
@@ -11,3 +11,32 @@ typedef struct {int aReflechir;}t_inv;
 typedef struct {char nom[20]; int niveau; t_caracteristique caract; t_equip equipement; t_inv inventaire;}t_personnage;
 
 typedef struct {int seed;  t_personnage perso; int difficulte;}t_partie;
+
+#include "deplacement.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void main(){
+	int i;
+	do{
+		/* Affichage du menu */
+		printf("\nMenu :\n");
+		printf(" 1 - Nouvelle partie\n");
+		printf(" 2 - Charger partie\n");
+		printf(" 3 - Test deplacement\n");
+		printf(" 4 - Test generation salles\n");
+		printf("Votre choix : ");
+		scanf("%i",&choix);
+
+		/* Traitement du choix de l'utilisateur */
+		switch(choix){	
+			case 1: printf("Pas encore fais");break;
+			case 2: printf("Pas encore fais");break;
+			case 3: testDeplacer(); break;
+			case 4: printf("Pas encore fais");break;
+			default: printf("Erreur: votre choix doit etre compris entre 1 et 6\n");
+		}
+	}while(choix!=6);
+	
+}
