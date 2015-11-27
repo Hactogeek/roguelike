15d7826cf8655dbfa603188969b6d18db37a488c
Marvin <callof61@gmail.com>
2015-11-27 10:24:33 +0100

    Ajout du module check, contient une verification d'arret sur la vie du personnage

---
+++ src/check.c
@@ -0,0 +1,14 @@
+void CheckAmorcer(){
+    
+}
+
+void CheckTester(){
+    
+}
+
+//Retourne 1 si la vie du personnage est a 0 ou moins cela retourne 1
+int CheckTestPersonnage(t_personnage perso){
+    return (perso.caract.vitalite<=0);
+}
+
+
