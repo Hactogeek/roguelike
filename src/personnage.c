1e72f4ec85ad58c8371e236ac1fdea4ae32226e6
hactogeek <tony.marteau@outlook.com>
2015-11-27 09:50:39 +0100

    PersonnageAfficher, PersonnageInitialiser

--- src/personnage.c
+++ src/personnage.c
@@ -1,49 +1,62 @@
-typedef struct {int contenuCase;}t_case;
-typedef struct {int x; int y;}t_posJoueur;
-typedef struct {t_case grille[N][M];t_posJoueur cord}t_carte;
-
-typedef struct {int vitalite; int force; int agilite;}t_caracteristique;
-typedef struct {int vie; int score;}t_stats;
-typedef struct {int arme; int armure;}t_equip;
-typedef struct {int aReflechir;}t_inv;
-
-typedef struct {char nom[20]; int niveau; t_stats statistique; t_caracteristique caract; t_equip equipement; t_inv inventaire;}t_personnage;
-
-typedef struct {int seed;  t_personnage perso; int difficulte;}t_partie;
-
+#include "../inc/general.h"
 
 void PersonnageAmorcer(){
 
 }
 
-void PersonnageInitialiser(t_personnage perso){
+void PersonnageInitialiser(t_personnage * perso){
 	char pseudo[20];
 	
-	printf("Création du Personnage\n");
-	
+    Appel0("PersonnageInitialiser");
+    printf("\nCréation Personnage\n");
+    
 	printf("Pseudo : ");
 	scanf("%20s", pseudo);
 	 
-	strcmp(perso.nom, pseudo);
-	perso.niveau=0;
-	perso.stats.vie=3;
-	perso.stats.score=1;
+    strcpy(perso->nom, pseudo);
+    
+	perso->niveau=0;
+	perso->stats.vie=3;
+	perso->stats.score=1;
+    
+    printf("Fin Creation Personnage\n\n");
+    Appel1("PersonnageInitialiser");
 }
 
-void PersonnageAfficher(t_personnage perso){
-	printf("Votre Personnage\n");
+void PersonnageAfficher(t_personnage * perso){
+	
+    Appel0("PersonnageAfficher");
+    
+    printf("\nVotre Personnage\n");
 	
 	printf("|Pseudo	|Niveau	|Vie	|Score	|\n");
-	printf("|	%s	|	%i	|	%i	|	%i	|\n", perso.nom, perso.niveau, perso.stats.vie, perso.stats.score);
+	printf("|	%s	|	%i	|	%i	|	%i	|\n\n", perso->nom, perso->niveau, perso->stats.vie, perso->stats.score);
+    
+    Appel1("PersonnageAfficher");
 }
 
-void PersonnageTester(/*int test*/){
+void PersonnageTester(){
 	int test;
 	t_personnage perso;
-	scanf("%i", &test);
-	switch(test){
-		case 1: PersonnageInitialiser(perso); break;
-		case 2: PersonnageAfficher(perso); break;
-	}
+    
+    printf("PersonnageTester : \n\n");
+    
+    do{
+        printf("\nMenu :\n");
+        printf(" 1 - Initialiser Personnage\n");
+        printf(" 2 - Afficher Personnage\n");
+        printf(" 3 - Sauvegarder Personnage\n");
+        printf(" 6 - Quitter\n\n");
+        
+        printf("Votre choix :");
+        scanf("%i", &test);
+        
+        switch(test){
+            case 1: PersonnageInitialiser(&perso); break;
+            case 2: PersonnageAfficher(&perso); break;
+            case 3: PersonnageSauvegarder(&perso); break;
+            default : printf("Erreur votre choix doit etre compris entre 1 et 6\n");
+        }
+    }while(test!=6);
 	
 }
