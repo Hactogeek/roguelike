9084e00dd0758571f990eab40af8718556856e5a
Dimitri Bernot <dimitri.bernot@yahoo.fr>
2015-11-27 09:50:19 +0100

    Correction deplacement.c

--- src/deplacement.c
+++ src/deplacement.c
@@ -4,11 +4,20 @@ void DeplacementAmorcer(){
 }
 
 void DeplacementTester(int t){
-    t_carte map;
-    switch(t){case 1 :Deplacement_marcher(map); break;}
+	t_carte map
+		switch(t){case 1 :Deplacement_marcher(map); break;}
 }
 
 /* Deplace le personnage qui est au position entré */
 void DeplacementMarcher(t_carte map){
+	
+	char cmd_move;
+	scanf("%c",&cmd_move);
+	switch(cmd_move){
+		case 'd': map.cord.x += (map.grille[map.cord.x+1][map.cord.y]!=1);break;
+		case 's': map.cord.y -= (map.grille[map.cord.x][map.cord.y-1]!=1);break;
+		case 'q': map.cord.x -= (map.grille[map.cord.x-1][map.cord.y]!=1);break;
+		case 'z': map.cord.y += (map.grille[map.cord.x][map.cord.y+1]!=1);break;
+	} // Effectue le deplacement et la verification de la position, le personnage ne peut pas etre dans un mur.
 }
 
