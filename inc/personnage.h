1c921df4a6322b3df833f4ac2cb6e263d654f063
Marvin <callof61@gmail.com>
2015-11-27 10:52:03 +0100

    stash

--- inc/personnage.h
+++ inc/personnage.h
@@ -1,4 +1,4 @@
 void PersonnageAmorcer();
-void PersonnageInitialiser(t_personnage perso);
-void PersonnageAfficher(t_personnage perso);
+void PersonnageInitialiser(t_personnage * perso);
+void PersonnageAfficher(t_personnage * perso);
 void PersonnageTester();
