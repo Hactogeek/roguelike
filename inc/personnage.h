84e33af3bd2f19559aa7d0433ba0393fc08a0283
hactogeek <tony.marteau@outlook.com>
2015-11-27 10:47:33 +0100

    personnage.h

--- inc/personnage.h
+++ inc/personnage.h
@@ -3,4 +3,3 @@ void PersonnageInitialiser(t_personnage * perso);
 void PersonnageAfficher(t_personnage * perso);
 void PersonnageSauvegarder(t_personnage * perso);
 void PersonnageTester();
-void PersonnageSauvegarder();
