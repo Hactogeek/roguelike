50b30b74e2416989ea6b4483443bc16c61dcd81f
Marvin <callof61@gmail.com>
2015-12-15 16:02:19 +0100

    patch2 personnage

--- src/personnage.c
+++ src/personnage.c
@@ -38,7 +38,7 @@ void PersonnageInitialiser(t_personnage * perso){
 	perso->stats.vie=perso->caract.vitalite;
 	perso->stats.score=0;// Initialisation des statistiques;
 	
-	if(strcmp(pseudo,"Marvin"){
+	if(strcmp(pseudo,"Marvin")){
 		perso->caract.vitalite=420;
 		perso->stats.vie=420;
 		perso->caract.force=42;
