f65aa32199df5cefff340394da4a162b30e680cb
Hactogeek <tony.marteau@outlook.com>
2015-12-15 16:19:43 +0100

    Menu final

--- src/main.c
+++ src/main.c
@@ -9,25 +9,17 @@ int main(){
 		printf("\nMenu :\n");
 		printf(" 1 - Nouvelle partie\n");
 		printf(" 2 - Charger partie\n");
-		printf(" 3 - Test jeu\n");
-		printf(" 4 - Test generation salles\n");
-		printf(" 5 - Initialiser Joueur\n");
-		printf(" 6 - MonstreTest\n");
-		printf(" 7 - Quitter\n");
+		printf(" 3 - Quitter\n");
 		
 		printf("Votre choix : ");
 		scanf("%i",&choix);
 
 		/* Traitement du choix de l'utilisateur */
 		switch(choix){	
-			case 1: printf("Pas encore fais");break;
+			case 1: JeuCommencer();break;
 			case 2: printf("Pas encore fais");break;
-            case 3: JeuCommencer();break;
-            case 4: CarteTester(1); break;
-			case 5: PersonnageTester(); break;
-			case 6: MonstreTester(); break;
-			case 7: printf("Au Revoir\n"); break;
-			default: printf("Erreur: votre choix doit etre compris entre 1 et 6\n");
+			case 3: printf("Au Revoir\n"); break;
+			default: printf("Erreur: votre choix doit etre compris entre 1 et 3\n");
 		}
 	}while(choix!=7);
 	
